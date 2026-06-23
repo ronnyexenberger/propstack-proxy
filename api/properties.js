471eaa8ec8d9c2ffd9388a39b639ca1c213fb7a1
Ronny Exenberger <mail@reell.at>
2026-06-23 10:58:29 +0200

    add propstack proxy

---
+++ api/properties.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  const response = await fetch('https://api.propstack.de/v1/units?expand=1', {
+    headers: {
+      'X-Api-Key': process.env.PROPSTACK_API_KEY
+    }
+  });
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
+}
